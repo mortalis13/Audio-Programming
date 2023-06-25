a51724daadb5089c1098b94b7438a259fb6a5e10
mortalis13 <stangerroman@gmail.com>
2023-06-25 21:29:21 +0200

    cleaned includes

--- ffplay.c
+++ ffplay.c
@@ -1,28 +1,14 @@
-#include "config.h"
-#include "config_components.h"
-#include <inttypes.h>
-#include <math.h>
-#include <limits.h>
 #include <signal.h>
 #include <stdint.h>
 
-#include "libavutil/avstring.h"
 #include "libavutil/channel_layout.h"
-#include "libavutil/eval.h"
 #include "libavutil/mathematics.h"
-#include "libavutil/pixdesc.h"
-#include "libavutil/imgutils.h"
 #include "libavutil/dict.h"
 #include "libavutil/fifo.h"
-#include "libavutil/parseutils.h"
 #include "libavutil/samplefmt.h"
 #include "libavutil/time.h"
 #include "libavutil/bprint.h"
 #include "libavformat/avformat.h"
-#include "libavdevice/avdevice.h"
-#include "libswscale/swscale.h"
-#include "libavutil/opt.h"
-#include "libavcodec/avfft.h"
 #include "libswresample/swresample.h"
 
 #include <SDL.h>
